f742d8988fca9c9c2ea7d6145e4e65526ae5d04d
gypsy9 <sziangos@gmail.com>
2017-03-06 17:51:14 -0800

    Deleted irrelevant comments

--- Test Code.rb
+++ Test Code.rb
@@ -1,8 +1,8 @@
 dealer_array= [[],[]]
 dealer_flat = []
 
-dealer_array[0] << 5 #(1 + Random.rand(10))
-dealer_array[1] << 6 #(1 + Random.rand(10))
+dealer_array[0] << 5 
+dealer_array[1] << 6
 dealer_array[1] << 7
 dealer_array[1] << 8
 
