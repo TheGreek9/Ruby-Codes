eeb478168b7c3d98b48fea27e1e4c0c5b8f7b90d
TheGreek9 <sziangos@gmail.com>
2017-03-11 09:50:38 -0800

    Changing values to show Maddie

--- Test Code.rb
+++ Test Code.rb
@@ -1,10 +1,10 @@
 dealer_array= [[],[]]
 dealer_flat = []
 
-dealer_array[0] << 5 
+dealer_array[0] << 5
+dealer_array[1] << 6
 dealer_array[1] << 6
 dealer_array[1] << 7
-dealer_array[1] << 8
 
 dealer_flat = dealer_array.flatten
 
